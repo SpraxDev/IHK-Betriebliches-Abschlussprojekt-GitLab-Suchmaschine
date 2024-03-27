99643774e7e97baae5ce75b7cd6e48e28480c56e
Christian Koop <contact@sprax2013.de>
2024-03-27 17:46:26 +0100

    feat: Add `updatedAt` and `createdAt` fields for files in the db schema
    These fields allow us to efficiently and easily delete old entries without keeping track
    of updated/created entries to only remove the other ones.

---
+++ prisma/migrations/20240327152424_add_created_and_updated_at_for_files/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `updated_at` to the `repository_files` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "files" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "repository_files" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
